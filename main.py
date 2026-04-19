62a87d82a114fa6802450636f62346fbf09c2177
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-04-19 09:23:55 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
-sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-kvadratlar = [son**2 for son in sonlar]
-print(kvadratlar)
+class Faktorial:
+    def __init__(self, son):
+        self.son = son
+
+    def faktorial_recursion(self):
+        if self.son == 0 or self.son == 1:
+            return 1
+        else:
+            return self.son * Faktorial(self.son - 1).faktorial_recursion()
+
+    def faktorial_loop(self):
+        faktorial = 1
+        for i in range(1, self.son + 1):
+            faktorial *= i
+        return faktorial
+
+def main():
+    while True:
+        print("1. Faktorialni recursion orqali hisoblash")
+        print("2. Faktorialni loop orqali hisoblash")
+        print("3. Chiqish")
+        tanlov = input("Tanlovni kiriting: ")
+        if tanlov == "1":
+            son = int(input("Sonni kiriting: "))
+            faktorial = Faktorial(son)
+            print(f"{son}! = {faktorial.faktorial_recursion()}")
+        elif tanlov == "2":
+            son = int(input("Sonni kiriting: "))
+            faktorial = Faktorial(son)
+            print(f"{son}! = {faktorial.faktorial_loop()}")
+        elif tanlov == "3":
+            break
+        else:
+            print("Noto'g'ri tanlov")
+
+if __name__ == "__main__":
+    main()
